ebdc19e4721af447129430338ebd33650aca53a3
Axel Hodler <axel@hodler.co>
2019-01-19 11:08:29 +0100

    handle upper constraints

--- src/test.js
+++ src/test.js
@@ -1,11 +1,12 @@
 const test = require('tape')
 
 class Constraints {
-  constructor(lowerBounds) {
+  constructor(lowerBounds, upperBounds) {
     this.lowerBounds = lowerBounds
+    this.upperBounds = upperBounds
   }
   isPositionViolatingConstrains(verticalPosition) {
-    return verticalPosition <= this.lowerBounds
+    return verticalPosition <= this.lowerBounds || verticalPosition >= this.upperBounds
   }
 }
 
@@ -15,7 +16,12 @@ class Paddle {
     this.verticalPosition = verticalPosition
   }
   moveUp() {
-    return new Paddle(this.constraints, this.verticalPosition + 1)
+    const verticalPositionAfter = this.verticalPosition + 1
+    if (this.constraints.isPositionViolatingConstrains(verticalPositionAfter)) {
+      return new Paddle(this.constraints, this.verticalPosition)
+    } else {
+      return new Paddle(this.constraints, verticalPositionAfter)
+    }
   }
   moveDown() {
     const verticalPositionAfter = this.verticalPosition - 1
@@ -31,7 +37,7 @@ class Paddle {
 }
 
 test('moving up moves the paddle up', assert => {
-  const c = new Constraints(0)
+  const c = new Constraints(0, 10)
   const p = new Paddle(c, 1)
   const movedPaddle = p.moveUp()
   assert.equal(movedPaddle.getVerticalPosition(), 2)
@@ -39,17 +45,25 @@ test('moving up moves the paddle up', assert => {
 })
 
 test('moving down moves the paddle down', assert => {
-  const c = new Constraints(0)
+  const c = new Constraints(0, 10)
   const p = new Paddle(c, 4)
   const movedPaddle = p.moveDown()
   assert.equal(movedPaddle.getVerticalPosition(), 3)
   assert.end()
 })
 
-test('paddle is not moved out of constraints', assert => {
-  const c = new Constraints(0)
+test('paddle is not moved out of lower constraints', assert => {
+  const c = new Constraints(0, 10)
   const p = new Paddle(c, 0)
   const movedPaddle = p.moveDown()
   assert.equal(movedPaddle.getVerticalPosition(), 0)
+  assert.end()
+})
+
+test('paddle is not moved out of upper constraints', assert => {
+  const c = new Constraints(0, 10)
+  const p = new Paddle(c, 10)
+  const movedPaddle = p.moveUp()
+  assert.equal(movedPaddle.getVerticalPosition(), 10)
   assert.end()
 })
