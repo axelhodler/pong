60cbcb8775552855d7fd055274e624639ef7f3ed
Axel Hodler <axel@hodler.co>
2019-01-19 10:16:24 +0100

    init paddle position

--- src/test.js
+++ src/test.js
@@ -1,6 +1,20 @@
 const test = require('tape')
 
-test('was set up', assert => {
-  assert.equal(true, true)
+class Paddle {
+  constructor(verticalPosition) {
+    this.verticalPosition = verticalPosition
+  }
+  moveUp() {
+    return new Paddle(this.verticalPosition + 1)
+  }
+  getVerticalPosition() {
+    return this.verticalPosition
+  }
+}
+
+test('moving up moves the paddle up', assert => {
+  const p = new Paddle(1)
+  const movedPaddle = p.moveUp()
+  assert.equal(movedPaddle.getVerticalPosition(), 2)
   assert.end()
 })
