c33e5fe98863c858a04fceecfe830d48896777c6
Axel Hodler <axel@hodler.co>
2019-01-19 10:18:09 +0100

    moving down example

--- src/test.js
+++ src/test.js
@@ -7,6 +7,9 @@ class Paddle {
   moveUp() {
     return new Paddle(this.verticalPosition + 1)
   }
+  moveDown() {
+    return new Paddle(this.verticalPosition - 1)
+  }
   getVerticalPosition() {
     return this.verticalPosition
   }
@@ -16,5 +19,12 @@ test('moving up moves the paddle up', assert => {
   const p = new Paddle(1)
   const movedPaddle = p.moveUp()
   assert.equal(movedPaddle.getVerticalPosition(), 2)
+  assert.end()
+})
+
+test('moving down moves the paddle down', assert => {
+  const p = new Paddle(4)
+  const movedPaddle = p.moveDown()
+  assert.equal(movedPaddle.getVerticalPosition(), 3)
   assert.end()
 })
