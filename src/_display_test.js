a76fe7cfc40ff0fa762cc0cdac0b2189d4e87087
Axel Hodler <axel@hodler.co>
2019-01-20 17:20:08 +0100

    some gamerendering

--- src/_display_test.js
+++ src/_display_test.js
@@ -6,15 +6,26 @@ class GameDisplay {
     this.paddle = paddle
   }
   render() {
-    return " \nI\n "
+    const gamespaces = [" ", " ", " "]
+    gamespaces[this.paddle.getVerticalPosition()] = "I"
+    return gamespaces.join("\n")
   }
 }
 
-test('moving up moves the paddle up', assert => {
+test('renders paddle in its row - 1', assert => {
   const c = new Constraints(0, 2)
   const p = new Paddle(c, 1)
   const game = new GameDisplay(p)
   const renderedGame = game.render()
   assert.equal(renderedGame, " \nI\n ")
+  assert.end()
+})
+
+test('renders paddle in its row - 2', assert => {
+  const c = new Constraints(0, 2)
+  const p = new Paddle(c, 0)
+  const game = new GameDisplay(p)
+  const renderedGame = game.render()
+  assert.equal(renderedGame, "I\n \n ")
   assert.end()
 })
