4cd34d5c51192e6f8efb059f94f505fd42aaa685
Axel Hodler <axel@hodler.co>
2019-01-27 11:30:02 +0100

    uncover rendering bug

--- src/_display_test.js
+++ src/_display_test.js
@@ -19,3 +19,30 @@ test('renders paddle in its row - 2', assert => {
   assert.equal(renderedGame, "I\n \n ")
   assert.end()
 })
+
+test('renders paddle in its row - 3', assert => {
+  const c = new Constraints(0, 2)
+  const p = new Paddle(c, 2)
+  const game = new GameDisplay()
+  const renderedGame = game.render(p)
+  assert.equal(renderedGame, " \n \nI")
+  assert.end()
+})
+
+test('renders paddle moving down', assert => {
+  const c = new Constraints(0, 2)
+  const p = new Paddle(c, 2)
+  const game = new GameDisplay()
+  const renderedGame = game.render(p.moveDown())
+  assert.equal(renderedGame, " \nI\n ")
+  assert.end()
+})
+
+test('renders paddle moving up', assert => {
+  const c = new Constraints(0, 2)
+  const p = new Paddle(c, 1)
+  const game = new GameDisplay()
+  const renderedGame = game.render(p.moveUp())
+  assert.equal(renderedGame, " \n \nI")
+  assert.end()
+})
